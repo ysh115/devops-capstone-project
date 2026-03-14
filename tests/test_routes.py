4e428a298a8cf32b67f5e546f91510dc2030712e
Yunbo Shi <yunboshi1022@gmail.com>
2026-03-14 02:33:08 -0400

    updates

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -49,6 +49,23 @@ class TestAccountService(TestCase):
     def tearDown(self):
         """Runs once after each test case"""
         db.session.remove()
+    
+    def test_get_account(self):
+        """It should Read a single Account"""
+        account = self._create_accounts(1)[0]
+        resp = self.client.get(
+            f"{BASE_URL}/{account.id}", content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(data["name"], account.name)
+    
+    def test_get_account_not_found(self):
+        """It should not Read an Account that is not found"""
+        resp = self.client.get(f"{BASE_URL}/0")
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+
 
     ######################################################################
     #  H E L P E R   M E T H O D S
