ddec74c5f1911615a6a6af54ebd8ab6b9bce3e29
Yunbo Shi <yunboshi1022@gmail.com>
2026-03-14 17:39:42 -0400

    linting

--- service/routes.py
+++ service/routes.py
@@ -57,6 +57,7 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
@@ -75,7 +76,6 @@ def list_accounts():
     return jsonify(account_list), status.HTTP_200_OK
 
 
-
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
@@ -92,7 +92,6 @@ def get_accounts(account_id):
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
 
     return account.serialize(), status.HTTP_200_OK
-
 
 
 ######################################################################
@@ -116,7 +115,6 @@ def update_accounts(account_id):
     return account.serialize(), status.HTTP_200_OK
 
 
-
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
@@ -135,11 +133,9 @@ def delete_accounts(account_id):
     return "", status.HTTP_204_NO_CONTENT
 
 
-
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
-
 
 def check_content_type(media_type):
     """Checks that the media type is correct"""
